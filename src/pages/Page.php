20e2f3290d33aa55a90682a6645512e8e1d9c691
nourdine <inghedebruin@yahoo.it>
2013-10-02 22:17:27 +0200

    dev

--- src/pages/Page.php
+++ src/pages/Page.php
@@ -3,5 +3,24 @@
 namespace pages;
 
 class Page {
-   
+
+   protected $url;
+
+   public function __construct($url = null) {
+      $this->url = $url;
+   }
+
+   public function getUrl() {
+      return $this->url;
+   }
+
+   public function setUrl($url) {
+      $this->url = $url;
+   }
+
+   public function exist() {
+      $headers = get_headers($this->url);
+      $status = $headers[0];
+      return strpos($status, "200") !== false;
+   }
 }
--- test/pages/PageTest.php
+++ test/pages/PageTest.php
@@ -4,7 +4,28 @@ use pages\Page;
 
 class PageTest extends PHPUnit_Framework_TestCase {
 
+   /**
+    * @var pages\Page
+    */
+   protected $page = null;
+
+   public function setUp() {
+      $this->page = new Page();
+   }
+
+   public function tearDown() {
+      $this->page = null;
+   }
+
    public function testExist() {
-      $this->assertTrue(true);
+      $this->page->setUrl("http://www.nourdine.net");
+      $bool = $this->page->exist();
+      $this->assertTrue($bool);
+   }
+
+   public function testNotExist() {
+      $this->page->setUrl("http://www.nxoxuxrxdxixnxe.net");
+      $bool = $this->page->exist();
+      $this->assertFalse($bool);
    }
 }
