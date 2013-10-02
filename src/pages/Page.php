fa95df9aa3e263f77428d7960c514d6e99d37908
nourdine <inghedebruin@yahoo.it>
2013-10-02 21:58:00 +0200

    dev

---
+++ src/pages/Page.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace pages;
+
+class Page {
+   
+}
---
+++ test/pages/PageTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use pages\Page;
+
+class PageTest extends PHPUnit_Framework_TestCase {
+
+   public function testExist() {
+      $this->assertTrue(true);
+   }
+}
