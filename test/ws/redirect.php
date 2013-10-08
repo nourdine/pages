54a2d79c3a8b60d932e7bfac718bcc5d08889b64
nourdine <inghedebruin@yahoo.it>
2013-10-08 14:30:35 +0200

    dev

--- test/ws/redirect.php
+++ test/ws/redirect.php
@@ -1,3 +1,18 @@
-<?php
+<?php header('Location: http://pages.ws.local/exist.php'); ?>
 
-header('Location: http://pages.ws.local/exist.php');
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="it" xml:lang="it">
+
+   <head>
+
+      <title>TITLE</title>
+
+      <meta name="description" content="DESCRIPTION" />
+
+   </head>
+
+   <body>
+
+   </body>
+
+</html>
--- test/ws/taking_long_time.php
+++ test/ws/taking_long_time.php
@@ -1,3 +1,18 @@
-<?php
+<?php sleep(6); ?>
 
-sleep(6);
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="it" xml:lang="it">
+
+   <head>
+
+      <title>TITLE</title>
+
+      <meta name="description" content="DESCRIPTION" />
+
+   </head>
+
+   <body>
+
+   </body>
+
+</html>
